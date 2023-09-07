60657e23ddcee1fc1ca221d9d9e31f8bd59affb6
Curtis <curtis@lassam.net>
2023-09-07 00:38:09 -0700

    vertical scrolling actually works really badly on iPhones

--- app.jsx
+++ app.jsx
@@ -3,16 +3,26 @@ import { createRoot } from "react-dom/client";
 import * as Server from 'react-dom/server'
 import Carousel from 'react-elastic-carousel'
 
-let Card = ({key, children}) => <div key={key} class='card'>{children}</div>
+let Card = ({key, children, ham}) => <div key={key} class={`card card-${ham}`}>
+    <div class="content">
+        {children}
+    </div>
+</div>
 
-let Greet = () => <div>
-        <Carousel itemsToShow={1}>
-            <Card key={1}>1</Card>
-            <Card key={2}>2</Card>
-            <Card key={3}>3</Card>
-            <Card key={4}>4</Card>
+let Null = () => <div></div>
+
+let Greet = () => {
+    let items = [];
+    for(let i = 0; i < 100; i++){
+        items.push(i)
+    }
+    let counter = 0;
+    return <div onWheel={(e)=>{console.warn(e.deltaY)}}>
+        <Carousel itemsToShow={1} renderArrow={Null} renderPagination={Null}>
+            {items.map((item) => <Card key={counter++} ham={counter++}>{item}</Card>)}
         </Carousel>
     </div>
+}
 console.log("Marts ahoy")
 
 const root = createRoot(document.getElementById("app"));
