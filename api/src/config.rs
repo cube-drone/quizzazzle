683f6fdfb14994e3516d7520d60b6655378ca597
Curtis <curtis@lassam.net>
2023-11-08 00:32:00 -0800

    rust stuffs

--- api/src/config.rs
+++ api/src/config.rs
@@ -1,11 +1,26 @@
-struct Config {
-    scyllaUrl: String,
+use std::env;
+
+pub struct Config {
+    pub scylla_url: String,
+    pub auth_redis_url: String,
+    pub cache_redis_url: String,
+    pub search_redis_url: String,
+    pub message_redis_url: String,
 }
 
-fn config() -> _ {
-    let scyllaUrl = env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
+pub fn config() -> Config {
+    let scylla_url = env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
+    let auth_redis_url = env::var("AUTH_REDIS_URI").unwrap_or_else(|_| "127.0.0.1:6032".to_string());
+    let cache_redis_url = env::var("CACHE_REDIS_URI").unwrap_or_else(|_| "127.0.0.1:6033".to_string());
+    let search_redis_url = env::var("SEARCH_REDIS_URI").unwrap_or_else(|_| "127.0.0.1:6034".to_string());
+    let message_redis_url = env::var("MESSAGE_REDIS_URI").unwrap_or_else(|_| "127.0.0.1:6035".to_string());
 
     let conf = Config {
-        scyllaUrl: scyllaUrl,
+        scylla_url: scylla_url,
+        auth_redis_url: auth_redis_url,
+        cache_redis_url: cache_redis_url,
+        search_redis_url: search_redis_url,
+        message_redis_url: message_redis_url,
     };
+    return conf;
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,31 @@
 #[macro_use] extern crate rocket;
+mod config;
+mod scylla_up;
+use std::sync::Arc;
+use rocket::State;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hooba")]
+fn hooba(services: &State<Services>) -> &'static str {
+    "Hooba, world!"
+}
+
+pub struct Services{
+    pub scylla: Arc<scylla_up::Session>,
+}
+
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+async fn rocket() -> _ {
+    let conf: config::Config = config::config();
+    let scylla = Arc::new(scylla_up::scylla_setup(&conf).await.unwrap());
+    let services = Services{
+        scylla: scylla,
+    };
+
+    rocket::build().manage(services)
+        .mount("/", routes![index, hooba])
 }
