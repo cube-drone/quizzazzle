f1cf9e3975b0546a91170df7c7c56ceba84020cd
Curtis Lassam <curtis@lassam.net>
2023-11-15 02:50:29 -0800

    lets do a json

--- js/test/data.js
+++ js/test/data.js
@@ -10,3 +10,16 @@ test('Can connect to localhost', async () => {
 
 	assert.strictEqual(text, "Hello world");
 });
+
+test('Can get some JSON', async () => {
+	let root = await fetch(`${endpoint}/basic/simple_json`);
+
+	let json = await root.json();
+
+	assert.strictEqual(json.username, "harbo");
+	let currentTimestamp = Date.now();
+	assert(json.timestamp_ms > (currentTimestamp - 10000));
+	assert(json.timestamp_ms < (currentTimestamp + 10000));
+
+	assert(json.active);
+});
