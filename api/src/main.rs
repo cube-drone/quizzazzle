981078b03c279df79b362cf099d024e0b7b00244
Curtis <curtis@lassam.net>
2023-11-18 10:36:24 -0800

    add scylla

--- api/src/main.rs
+++ api/src/main.rs
@@ -6,6 +6,8 @@ use std::sync::Arc;
 use redis::cluster::ClusterClient;
 use redis::AsyncCommands;
 use rocket::{Rocket, Build};
+use scylla::transport::session::{Session};
+use scylla::SessionBuilder;
 
 mod error; // provides the no_shit! macro
 mod basic;
@@ -19,7 +21,7 @@ fn index() -> &'static str {
 pub struct Services{
     pub cache_redis: Arc<ClusterClient>,
     pub application_redis: Arc<ClusterClient>,
-    //pub scylla: Arc<scylla_up::Session>,
+    pub scylla: Arc<Session>,
 }
 
 async fn setup_redis_cluster(redis_urls: &String) -> Arc<ClusterClient> {
@@ -35,9 +37,16 @@ async fn setup_redis_cluster(redis_urls: &String) -> Arc<ClusterClient> {
     let result: String = connection.get("foo").await.expect("Could not test Redis connection");
     assert_eq!(result, "bar");
 
-    return Arc::new(client);
+    Arc::new(client)
 }
 
+async fn setup_scylla_cluster(scylla_url: &String) -> Arc<Session> {
+    let session: Session = SessionBuilder::new().known_node(scylla_url).build().await.expect("Could not connect to Scylla");
+
+    session.query("CREATE KEYSPACE IF NOT EXISTS ks WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'replication_factor' : 2}", &[]).await.expect("Could not create keyspace");
+
+    Arc::new(session)
+}
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
@@ -46,10 +55,14 @@ async fn rocket() -> Rocket<Build> {
     let cache_redis_urls = env::var("CACHE_REDIS_URLS").unwrap_or_else(|_| "".to_string());
     let application_redis_urls = env::var("APPLICATION_REDIS_URLS").unwrap_or_else(|_| "".to_string());
 
+    // Scylla Setup
+    let scylla_url = env::var("SCYLLA_URL").unwrap_or_else(|_| "".to_string());
+
     // Service Setup
     let services = Services{
         cache_redis: setup_redis_cluster(&cache_redis_urls).await,
         application_redis: setup_redis_cluster(&application_redis_urls).await,
+        scylla: setup_scylla_cluster(&scylla_url).await,
     };
 
     let mut app = rocket::build();
