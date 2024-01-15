e4192f691651f7fb7e8d38f135684c14b7fb62e4
Curtis <curtis@lassam.net>
2024-01-15 02:58:01 -0800

    some nav

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ function debounce(func, timeout = 300){
     };
 }
 
+let bootupTime = Date.now();
+
 const text = `
 ## Markdown Markdown
 
@@ -39,7 +41,7 @@ let visibilityObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         let id = entry.target.id;
         if(entry.isIntersecting){
-            console.warn(`showing: ${id}`);
+            //console.warn(`showing: ${id}`);
             entry.target.classList.add('currently-visible');
             if(showHandlers[id]){
                 showHandlers[id]();
@@ -64,7 +66,7 @@ let primaryObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         let id = entry.target.id;
         if(entry.isIntersecting){
-            console.warn(`showing: ${id}`);
+            //console.warn(`showing: ${id}`);
             entry.target.classList.add('currently-visible');
             if(primaryShowHandlers[id]){
                 primaryShowHandlers[id]();
@@ -109,19 +111,13 @@ class VisibilityTrigger extends Component {
         }
     }
 
-    hello(){
-        console.warn("hello");
-    }
-
     visible(){
-        console.log("hi");
         this.setState({
             visible: true
         });
     }
 
     invisible(){
-        console.log("bye");
         this.setState({
             visible: false
         });
@@ -175,24 +171,81 @@ class VisibilityTrigger extends Component {
     }
 }
 
-let index = [
-    {
-        id: "1",
+let index = [];
+for(let i = 0; i < 100; i++){
+    index.push({
+        id: i,
         type: "text",
-        content: "hi"
-    },
-    {
-        id: "2",
-        type: "text",
-        content: "hello"
-    },
-    {
-        id: "3",
-        type: "text",
-        content: "hoyo"
-    }
-]
+        content: `hi ${i}`
+    });
+}
 
+function Icon({name}){
+    if(name == "chevron-down"){
+        return html`<svg viewBox="0 0 64 64" class="svg-icon icon-circle-chevron-down">
+            <path class="opt" d="M32,1.8C15.3,1.8,1.8,15.3,1.8,32S15.3,62.3,32,62.3S62.3,48.7,62.3,32S48.7,1.8,32,1.8z M32,57.8
+                C17.8,57.8,6.3,46.2,6.3,32C6.3,17.8,17.8,6.3,32,6.3c14.2,0,25.8,11.6,25.8,25.8C57.8,46.2,46.2,57.8,32,57.8z"/>
+            <path d="M43.9,23.8L32,35.5L20.1,23.8c-0.9-0.9-2.3-0.9-3.2,0c-0.9,0.9-0.9,2.3,0,3.2l13.5,13.3c0.4,0.4,1,0.6,1.6,0.6
+                c0.6,0,1.1-0.2,1.6-0.6L47.1,27c0.9-0.9,0.9-2.3,0-3.2C46.3,22.9,44.8,22.9,43.9,23.8z"/>
+        </svg>`;
+    }
+    if(name == "chevron-up"){
+        return html`<svg viewBox="0 0 64 64" class="svg-icon icon-circle-chevron-up">
+            <path class="opt" d="M32,1.8C15.3,1.8,1.8,15.3,1.8,32S15.3,62.3,32,62.3S62.3,48.7,62.3,32S48.7,1.8,32,1.8z M32,57.8
+                C17.8,57.8,6.3,46.2,6.3,32C6.3,17.8,17.8,6.3,32,6.3c14.2,0,25.8,11.6,25.8,25.8C57.8,46.2,46.2,57.8,32,57.8z"/>
+            <path d="M33.6,23.8c-0.9-0.9-2.3-0.9-3.2,0L16.9,37c-0.9,0.9-0.9,2.3,0,3.2c0.4,0.4,1,0.7,1.6,0.7c0.6,0,1.1-0.2,1.6-0.6L32,28.5
+                l11.9,11.7c0.9,0.9,2.3,0.9,3.2,0c0.9-0.9,0.9-2.3,0-3.2L33.6,23.8z"/>
+        </svg>`;
+    }
+    if(name == "question"){
+        return html`<svg viewBox="0 0 64 64" class="svg-icon icon-circle-question">
+            <path class="opt" d="M32,1.8C15.3,1.8,1.8,15.3,1.8,32S15.3,62.3,32,62.3S62.3,48.7,62.3,32S48.7,1.8,32,1.8z M32,57.8
+                C17.8,57.8,6.3,46.2,6.3,32C6.3,17.8,17.8,6.3,32,6.3c14.2,0,25.8,11.6,25.8,25.8C57.8,46.2,46.2,57.8,32,57.8z"/>
+            <path d="M33.8,12.1c-2.9-0.5-5.9,0.3-8.1,2.2c-2.2,1.9-3.5,4.6-3.5,7.6c0,1.1,0.2,2.2,0.6,3.3c0.4,1.2,1.7,1.8,2.9,1.4
+                c1.2-0.4,1.8-1.7,1.4-2.9c-0.2-0.6-0.3-1.2-0.3-1.8c0-1.6,0.7-3.1,1.9-4.1c1.2-1,2.8-1.5,4.5-1.2c2.1,0.4,3.9,2.2,4.3,4.3
+                c0.4,2.5-0.9,5-3.2,6c-2.6,1.1-4.3,3.7-4.3,6.7v6.2c0,1.2,1,2.3,2.3,2.3c1.2,0,2.3-1,2.3-2.3v-6.2c0-1.1,0.6-2.1,1.5-2.5
+                c4.3-1.8,6.8-6.3,6-10.9C41,16.1,37.8,12.8,33.8,12.1z"/>
+            <path d="M32.1,45.8h-0.3c-1.2,0-2.3,1-2.3,2.3s1,2.3,2.3,2.3h0.3c1.2,0,2.2-1,2.2-2.3S33.4,45.8,32.1,45.8z"/>
+        </svg>`;
+    }
+    else{
+        return html`<svg viewBox="0 0 64 64" class="svg-icon icon-not-found">
+            <path class="opt" d="M32,1.8C15.3,1.8,1.8,15.3,1.8,32S15.3,62.3,32,62.3S62.3,48.7,62.3,32S48.7,1.8,32,1.8z M32,57.8
+                C17.8,57.8,6.3,46.2,6.3,32C6.3,17.8,17.8,6.3,32,6.3c14.2,0,25.8,11.6,25.8,25.8C57.8,46.2,46.2,57.8,32,57.8z"/>
+            <path d="M33.8,12.1c-2.9-0.5-5.9,0.3-8.1,2.2c-2.2,1.9-3.5,4.6-3.5,7.6c0,1.1,0.2,2.2,0.6,3.3c0.4,1.2,1.7,1.8,2.9,1.4
+                c1.2-0.4,1.8-1.7,1.4-2.9c-0.2-0.6-0.3-1.2-0.3-1.8c0-1.6,0.7-3.1,1.9-4.1c1.2-1,2.8-1.5,4.5-1.2c2.1,0.4,3.9,2.2,4.3,4.3
+                c0.4,2.5-0.9,5-3.2,6c-2.6,1.1-4.3,3.7-4.3,6.7v6.2c0,1.2,1,2.3,2.3,2.3c1.2,0,2.3-1,2.3-2.3v-6.2c0-1.1,0.6-2.1,1.5-2.5
+                c4.3-1.8,6.8-6.3,6-10.9C41,16.1,37.8,12.8,33.8,12.1z"/>
+            <path d="M32.1,45.8h-0.3c-1.2,0-2.3,1-2.3,2.3s1,2.3,2.3,2.3h0.3c1.2,0,2.2-1,2.2-2.3S33.4,45.8,32.1,45.8z"/>
+        </svg>`;
+    }
+
+}
+
+function Nav({onTop, onBottom}){
+    if(!onTop){
+        onTop = () => {};
+    }
+    if(!onBottom){
+        onBottom = () => {};
+    }
+
+    return html`<nav id="primary-nav">
+        <ul>
+            <li>
+                <a id="end" onClick=${onBottom} title="end">
+                    <${Icon} name="chevron-down" />
+                </a>
+            </li>
+            <li>
+                <a id="end" onClick=${onTop} title="beginning">
+                    <${Icon} name="chevron-up" />
+                </a>
+            </li>
+        </ul>
+    </nav>`;
+
+}
 class App extends Component {
 
     constructor(props){
@@ -215,11 +268,11 @@ class App extends Component {
                 });
             })
             if(this.lastScrollTop > scrollTop && this.state.scrollDirection != "backward"){
-                console.warn("scrolling backward");
+                //console.warn("scrolling backward");
                 changeDirection("backward");
             }
             else if(this.lastScrollTop < scrollTop && this.state.scrollDirection != "forward"){
-                console.warn("scrolling forward");
+                //console.warn("scrolling forward");
                 changeDirection("forward");
 
                 this.setState({
@@ -232,21 +285,39 @@ class App extends Component {
         })
     }
 
+    goToTop(){
+        let element = this.base;
+        let everything = element.querySelector('.everything-feed');
+        everything.scrollTop = 0;
+    }
+
+    goToBottom(){
+        let element = this.base;
+        let everything = element.querySelector('.everything-feed');
+        everything.scrollTop = everything.scrollHeight;
+    }
+
     render(){
+        let justBooted = Date.now() - bootupTime < 5000;
         let headerVisible = this.state.scrollDirection == "backward" ? "header-visible" : "header-invisible";
+        if(justBooted){
+            headerVisible = "header-visible";
+        }
         let disableTransparentIcons = this.lastScrollTop > 60 ? "disable-transparent-icons" : "";
+
+        let items = index.map((item) => {
+            return html`<${VisibilityTrigger} />`;
+        });
 
         return html`<div class="primary-card">
             <div class="content">
                 <header class="${headerVisible} ${disableTransparentIcons}">
-                    hi
+                    <${Nav} onTop=${this.goToTop.bind(this)} onBottom=${this.goToBottom.bind(this)}/>
                 </header>
                 <div class="everything-feed">
                     <h2>Hi!</h2>
                     <div class="frames">
-                    <${VisibilityTrigger} />
-                    <${VisibilityTrigger} />
-                    <${VisibilityTrigger} />
+                    ${items}
                     </div>
                 </div>
             </div>
