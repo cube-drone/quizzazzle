70f3b68f684b79801b4e824c363766c5a6fc98fa
Curtis <curtis@lassam.net>
2024-01-15 02:03:31 -0800

    fussing with a thingie

--- js/app.js
+++ js/app.js
@@ -4,8 +4,16 @@ import htm from 'htm';
 import { marked } from 'marked';
 import insane from 'insane';
 import { hash128 } from './lib/murmurhash3.js'
+import { v4 as uuid } from "uuid";
 import anime from 'animejs'
-import ZingTouch from 'zingtouch';
+
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
 
 const text = `
 ## Markdown Markdown
@@ -25,38 +33,226 @@ let Rendered = () => {
     return html`<div class="hi" dangerouslySetInnerHTML=${{ __html: rendered }}></div>`;
 }
 
-let Quiz = () => {
-    let [index, setIndex] = useState(0);
-    let items = [];
-    for(let i = 0; i < 100; i++){
-        items.push(i)
-    }
-    let counter = 0;
-    return html`<div class="card">
-        <div class="content">
-            <div class="header">
-                <div class="prev">«</div>
-                <div class="more">…</div>
-                <div class="next">»</div>
-            </div>
-            <div class="bump"></div>
-            <div class="everything">
-                <h2>Hi!</h2>
-                <${Rendered} />
-            </div>
-            <div class="bump"></div>
-            <div class="footer"></div>
-        </div>
-    </div>`
+let showHandlers = {};
+let hideHandlers = {};
+let visibilityObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        let id = entry.target.id;
+        if(entry.isIntersecting){
+            console.warn(`showing: ${id}`);
+            entry.target.classList.add('currently-visible');
+            if(showHandlers[id]){
+                showHandlers[id]();
+            }
+        }
+        else{
+            entry.target.classList.remove('currently-visible');
+            if(hideHandlers[id]){
+                hideHandlers[id]();
+            }
+        }
+    });
+}
+, {
+    root: null, // use the viewport
+    rootMargin: '0px', // anywhere in the viewport
+    threshold: 0.1 // a tiny fraction of the element must be visible
+});
+let primaryShowHandlers = {};
+let primaryHideHandlers = {};
+let primaryObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        let id = entry.target.id;
+        if(entry.isIntersecting){
+            console.warn(`showing: ${id}`);
+            entry.target.classList.add('currently-visible');
+            if(primaryShowHandlers[id]){
+                primaryShowHandlers[id]();
+            }
+        }
+        else{
+            entry.target.classList.remove('currently-visible');
+            if(primaryHideHandlers[id]){
+                primaryHideHandlers[id]();
+            }
+        }
+    });
+}
+, {
+    root: null, // use the viewport
+    rootMargin: '0px', // anywhere in the viewport
+    threshold: 0.9 // 100% of the element must be visible
+});
+function observe(id, element, showHandler, hideHandler, primaryShowHandler, primaryHideHandler){
+    showHandlers[id] = showHandler;
+    hideHandlers[id] = hideHandler;
+    primaryShowHandlers[id] = primaryShowHandler;
+    primaryHideHandlers[id] = primaryHideHandler;
+    visibilityObserver.observe(element);
+    primaryObserver.observe(element);
+}
+function unobserve(id, element){
+    delete showHandlers[id];
+    delete hideHandlers[id];
+    delete primaryShowHandlers[id];
+    delete primaryHideHandlers[id];
+    visibilityObserver.unobserve(element);
+    primaryObserver.unobserve(element);
 }
 
-let Nav = () => {
-    return html`<nav>
-        <div class="nav-item">Home</div>
-        <div class="nav-item">About</div>
-    </nav>`
+class VisibilityTrigger extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            visible: false,
+            primary: false
+        }
+    }
+
+    hello(){
+        console.warn("hello");
+    }
+
+    visible(){
+        console.log("hi");
+        this.setState({
+            visible: true
+        });
+    }
+
+    invisible(){
+        console.log("bye");
+        this.setState({
+            visible: false
+        });
+    }
+
+    primary(){
+        this.setState({
+            primary: true
+        });
+    }
+
+    unprimary(){
+        this.setState({
+            primary: false
+        });
+    }
+
+    componentDidMount(){
+        let element = this.base;
+        this.id = uuid();
+        element.id = this.id;
+        observe(this.id, element,
+            this.visible.bind(this),
+            this.invisible.bind(this),
+            this.primary.bind(this),
+            this.unprimary.bind(this)
+        );
+    }
+
+    componentWillUnmount(){
+        let element = this.base;
+        unobserve(this.id, element);
+    }
+
+    render(){
+        if(this.state.primary){
+            return html`<div class="frame frame-primary">
+                primary
+            </div>`;
+        }
+        if(this.state.visible){
+            return html`<div class="frame frame-visible">
+                visible
+            </div>`;
+        }
+        else{
+            return html`<div class="frame frame-invisible">
+                invisible
+            </div>`;
+        }
+    }
+}
+
+let index = [
+    {
+        id: "1",
+        type: "text",
+        content: "hi"
+    },
+    {
+        id: "2",
+        type: "text",
+        content: "hello"
+    },
+    {
+        id: "3",
+        type: "text",
+        content: "hoyo"
+    }
+]
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.lastScrollTop;
+        this.state = {
+            scrollDirection: "backward",
+        }
+    }
+
+    componentDidMount(){
+        let element = this.base;
+        let everything = element.querySelector('.everything');
+        everything.addEventListener('scroll', (e) => {
+            let scrollTop = everything.scrollTop;
+            let changeDirection = debounce((direction) => {
+                this.setState({
+                    scrollDirection: direction
+                });
+            })
+            if(this.lastScrollTop > scrollTop && this.state.scrollDirection != "backward"){
+                console.warn("scrolling backward");
+                changeDirection("backward");
+            }
+            else if(this.lastScrollTop < scrollTop && this.state.scrollDirection != "forward"){
+                console.warn("scrolling forward");
+                changeDirection("forward");
+
+                this.setState({
+                    scrollDirection: "forward"
+                });
+            }
+            else{
+            }
+            this.lastScrollTop = scrollTop;
+        })
+    }
+
+    render(){
+        let headerVisible = this.state.scrollDirection == "backward" ? "header-visible" : "header-invisible";
+        let tall = this.state.scrollDirection == "backward" ? "" : "tall";
+        return html`<div class="primary-card">
+            <div class="content">
+                <header class="${headerVisible}">
+                    hi
+                </header>
+                <div class="everything ${tall}">
+                    <h2>Hi!</h2>
+                    <div class="frames">
+                    <${VisibilityTrigger} />
+                    <${VisibilityTrigger} />
+                    <${VisibilityTrigger} />
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+
 }
 
 console.log(hash128(rendered));
 
-render(html`<${Quiz} name="World" />`, document.getElementById('app'));
+render(html`<${App} />`, document.getElementById('app'));
