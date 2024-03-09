8f7e00a39f8bb20729227b89844f85f3426202ab
Curtis Lassam <curtis@lassam.net>
2024-03-09 09:03:59 -0800

    fix the last test we are now running singlenode baby

--- js/test/constants.js
+++ js/test/constants.js
@@ -1,3 +1,6 @@
+// https://stackoverflow.com/questions/72390154/econnrefused-when-making-a-request-to-localhost-using-fetch-in-node-js
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 
 let endpoint = process.env.LOCAL_ENDPOINT ?? 'http://127.0.0.1:3333';
 
